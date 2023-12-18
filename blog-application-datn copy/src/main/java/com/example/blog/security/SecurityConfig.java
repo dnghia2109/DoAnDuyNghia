dcae4b811c8018c6c200dbb7e8bb05308ad881b4
Lai Duy Nghia <laiduynghia21@gmail.com>
2023-12-18 23:03:06 +0700

    Update code security handle 18/12/23 v4

--- blog-application-datn copy/src/main/java/com/example/blog/security/SecurityConfig.java
+++ blog-application-datn copy/src/main/java/com/example/blog/security/SecurityConfig.java
@@ -165,7 +165,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers("/admin-assets/**","/admin-lte/**","/assets/**", "/css/**", "/img/**",
                         "/js/**", "/vendor/**" ,"/error/**").permitAll()
-            .antMatchers("/homepage/**", "/admin/login", "/api/v1/**", "/blogs", "/register/**").permitAll()
+            .antMatchers("/homepage/**", "/admin/login", "/api/v1/**", "/blogs", "/register/**"
+                    , "/feedback", "/api/v1/feedback/**").permitAll()
             .antMatchers("/dashboard/blogs/own-blogs",
                          "/dashboard/blogs/create", "/dashboard/blogs/{id}/detail",
                          "/api/v1/admin/blogs/**")
